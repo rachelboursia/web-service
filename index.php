b9c73accefd24e5552b1b3178d03c83995937136
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2022-02-21 12:50:44 -0800

    Ad ds repl.it configuration files and index

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>PHP Page!</title>
+</head>
+<body>
+    <h1>PHP Page!</h1>
+    <p>This is HTML</p>
+    <?php
+        echo "<p>This is PHP</p>";
+    ?>
+    <p>This is HTML</p>
+</body>
+</html>
